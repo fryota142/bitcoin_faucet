9a85f86eaea6102d991055ea9a1754e10628bcaa
Ryota Fukata <fukata@haw.co.jp>
2020-11-19 01:52:47 +0900

    conect bitcoin core and view amount

--- app/controllers/faucet_controller.rb
+++ app/controllers/faucet_controller.rb
@@ -1,4 +1,12 @@
 class FaucetController < ApplicationController
+  before_action :bitcoin_rpc
+
+  def bitcoin_rpc
+    config = { schema: 'http', host: 'localhost', port: 18443, user: 'user', password: 'pass' }
+    @client = Bitcoin::RPC::BitcoinCoreClient.new(config)
+  end
+
   def index
+    @amount = @client.listunspent.sum { |utxo| utxo['amount'].to_f}
   end
 end
